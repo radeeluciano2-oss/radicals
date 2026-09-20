b6cb3ce4357eea08ab566713579d2c761460b131
radeeluciano2-oss <radeeluciano2@gmail.com>
2026-09-20 23:21:41 +0800

    Remove unused React import causing Deno lint failure

--- main.jsx
+++ main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
